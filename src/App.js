aa54a916053f2b630aad521ce277aa38995554ef
Ho Van Phong <phongckm93@gmail.com>
2022-08-24 14:19:17 +0700

    complete

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 
 import './App.css';
 import ShopRedux from './Ex_shoes_shop _reduce/ShopRedux';
-// import Ex_shop_redux from './Ex_shoes_shop _reduce/Ex_shop_redux';
 function App() {
 
   return (
