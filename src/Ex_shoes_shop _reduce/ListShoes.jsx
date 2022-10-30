4538fd4248741e300ebc31ef2f1763c1b0e78851
Ho Van Phong <phongckm93@gmail.com>
2022-10-31 01:47:49 +0700

    changed shoeReducer

--- src/Ex_shoes_shop _reduce/ListShoes.jsx
+++ src/Ex_shoes_shop _reduce/ListShoes.jsx
@@ -1,41 +1,80 @@
 import React, { Component } from "react";
-
+import axios from "axios";
 import ItemShoes from "./ItemShoes";
 import { connect } from "react-redux";
+const BASE_URL = "https://62db6ca4d1d97b9e0c4f338f.mockapi.io";
+let initialization = [
+    {
+        id: 1,
+        name: "Adidas Prophere",
+        alias: "adidas-prophere",
+        price: 350,
+        description:
+            "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+        shortDescription:
+            "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+        quantity: 995,
+        image: "http://svcy3.myclass.vn/images/adidas-prophere.png",
+    },
+];
 class ListShoes extends Component {
-  render() {
-    let { name, price, description } = this.props.detailShoe;
-    return (
-      <div className="container">
-        <div className="row justify-content-center">
-          {this.props.shoeArr.map((item, i) => {
-            return (
-              <div key={i} className="col-4">
-                <ItemShoes detail={item} />
-              </div>
-            );
-          })}
-        </div>
-        <div id="info" className="container row mt-5">
-          <img className="col-4" src={this.props.detailShoe.image} alt="" />
-          <p className="col-8 text-left text-success">
-            <span className="text-dark font-weight-bold">{name}</span>
-            <br />
-            <span className="text-danger">{price}$</span>
-            <br />
-            <span className="text-secondary">{description}</span>
-          </p>
-        </div>
-      </div>
-    );
-  }
+    state = {
+        shoeArr: initialization,
+    };
+    async getData() {
+        try {
+            let { data } = await axios({
+                url: `${BASE_URL}/shoeShop`,
+                method: "GET",
+            });
+            this.setState({
+                shoeArr: data,
+            });
+        } catch (error) {
+            console.log("error: ", error);
+        }
+    }
+    componentDidMount() {
+        this.getData();
+    }
+    render() {
+        let { name, price, description } = this.props.detailShoe;
+        return (
+            <div className="container">
+                <div className="row justify-content-center">
+                    {this.state.shoeArr.map((item, i) => {
+                        return (
+                            <div key={i} className="col-4">
+                                <ItemShoes detail={item} />
+                            </div>
+                        );
+                    })}
+                </div>
+                <div id="info" className="container row mt-5">
+                    <img
+                        className="col-4"
+                        src={this.props.detailShoe.image}
+                        alt=""
+                    />
+                    <p className="col-8 text-left text-success">
+                        <span className="text-dark font-weight-bold">
+                            {name}
+                        </span>
+                        <br />
+                        <span className="text-danger">{price}$</span>
+                        <br />
+                        <span className="text-secondary">{description}</span>
+                    </p>
+                </div>
+            </div>
+        );
+    }
 }
 
 const mapStateToProps = (state) => {
-  return {
-    shoeArr: state.shoeReducer.shoeArr,
-    detailShoe: state.shoeReducer.detailShoe,
-  };
+    return {
+        detailShoe: state.shoeReducer.detailShoe,
+    };
 };
 
 export default connect(mapStateToProps, null)(ListShoes);
--- src/Ex_shoes_shop _reduce/redux/reducer/shoeReducer.jsx
+++ src/Ex_shoes_shop _reduce/redux/reducer/shoeReducer.jsx
@@ -1,86 +1,78 @@
-import { shoeArr } from "../../data";
 import {
-  ADD,
-  BUY,
-  DECREASE,
-  DETAIL,
-  INCREASE,
-  REMOVE,
+    ADD,
+    BUY,
+    DECREASE,
+    DETAIL,
+    INCREASE,
+    REMOVE,
 } from "./constants/shoeConstants";
-
+let initialization = [
+    {
+        id: 1,
+        name: "Adidas Prophere",
+        alias: "adidas-prophere",
+        price: 350,
+        description:
+            "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
+        shortDescription:
+            "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
+        quantity: 995,
+        image: "http://svcy3.myclass.vn/images/adidas-prophere.png",
+    },
+];
 let initialState = {
-  shoeArr: shoeArr,
-  detailShoe: shoeArr[0],
-  gioHang: [],
+    detailShoe: initialization[0],
+    gioHang: [],
 };
 export let shoeReducer = (state = initialState, { type, payload }) => {
-  switch (type) {
-    case DETAIL:
-      {
-        let newDetailShoe = { ...state.detailShoe };
-        let i = state.shoeArr.findIndex((item) => {
-          return item.id === payload.id;
-        });
-        newDetailShoe = shoeArr[i];
-        state.detailShoe = newDetailShoe;
-      }
-      return { ...state };
-
-    case ADD:
-      {
-        let newGioHang = [...state.gioHang];
-        let i = state.gioHang.findIndex((item) => {
-          return item.id === payload.id;
-        });
-        if (i === -1) {
-          let newItem = { ...payload, soLuong: 1 };
-          newGioHang.push(newItem);
-        } else newGioHang[i].soLuong++;
-
-        state.gioHang = newGioHang;
-      }
-      return { ...state };
-    case INCREASE:
-      {
-        let newGioHang = [...state.gioHang];
-        let i = state.gioHang.findIndex((item) => {
-          return item.id === payload.id;
-        });
-        newGioHang[i].soLuong++;
-        state.gioHang = newGioHang;
-      }
-      return { ...state };
-    case DECREASE:
-      {
-        let newGioHang = [...state.gioHang];
-        let i = state.gioHang.findIndex((item) => {
-          return item.id === payload.id;
-        });
-        if (newGioHang[i].soLuong > 1) {
-          newGioHang[i].soLuong--;
+    switch (type) {
+        case DETAIL:
+            return { ...state, detailShoe: payload };
+        case ADD: {
+            let newGioHang = [...state.gioHang];
+            let i = state.gioHang.findIndex((item) => {
+                return item.id === payload.id;
+            });
+            if (i === -1) {
+                let newItem = { ...payload, soLuong: 1 };
+                newGioHang.push(newItem);
+            } else newGioHang[i].soLuong++;
+            return { ...state, gioHang: newGioHang };
         }
-        state.gioHang = newGioHang;
-      }
-      return { ...state };
-    case REMOVE:
-      {
-        let newGioHang = [...state.gioHang];
-        let i = state.gioHang.findIndex((item) => {
-          return item.id === payload.id;
-        });
-        newGioHang.splice(i, 1);
-        state.gioHang = newGioHang;
-      }
-      return { ...state };
-    case BUY:
-      {
-        let newGioHang = [...state.gioHang];
-        newGioHang.splice(0);
-        state.gioHang = newGioHang;
-      }
-      alert("Chúc mừng bạn đặt hàng thành công");
-      return { ...state };
-    default:
-      return state;
-  }
+        case INCREASE: {
+            let newGioHang = [...state.gioHang];
+            let i = state.gioHang.findIndex((item) => {
+                return item.id === payload.id;
+            });
+            newGioHang[i].soLuong++;
+            return { ...state, gioHang: newGioHang };
+        }
+        case DECREASE: {
+            let newGioHang = [...state.gioHang];
+            let i = state.gioHang.findIndex((item) => {
+                return item.id === payload.id;
+            });
+            if (newGioHang[i].soLuong > 1) {
+                newGioHang[i].soLuong--;
+            }
+            return { ...state, gioHang: newGioHang };
+        }
+        case REMOVE: {
+            let newGioHang = [...state.gioHang];
+            let i = state.gioHang.findIndex((item) => {
+                return item.id === payload.id;
+            });
+            newGioHang.splice(i, 1);
+            return { ...state, gioHang: newGioHang };
+        }
+        case BUY: {
+            let newGioHang = [...state.gioHang];
+            newGioHang.splice(0);
+            alert("Chúc mừng bạn đặt hàng thành công");
+            return { ...state, gioHang: newGioHang };
+        }
+
+        default:
+            return state;
+    }
 };
